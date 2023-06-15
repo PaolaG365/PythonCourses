6ac03c5371c218b4b00c1804f57ae1c89d36929e
PaolaG365 <paola.georgieva.365@gmail.com>
2023-06-15 22:22:02 +0300

    more exercises

--- PythonFundamentalsMay2023/5.Lists_advanced/2.Exercises/2.Next_version.py
+++ PythonFundamentalsMay2023/5.Lists_advanced/2.Exercises/2.Next_version.py
@@ -2,3 +2,14 @@ version_program = input().split(".")
 next_version = int("".join(version_program)) + 1
 result = str(next_version)
 print(*result, sep=".")
+
+# second version
+# version_program = [int(x) for x in input().split(".")]
+# version_program[-1] += 1
+#
+# for version in range(len(version_program) - 1, - 1, - 1):
+#     if version_program[version] > 9 and version - 1 >= 0:
+#         version_program[version] = 0
+#         version_program[version - 1] += 1
+#
+# print(*version_program, sep=".")
--- PythonFundamentalsMay2023/5.Lists_advanced/2.Exercises/9.Anonymous_threat.py
+++ PythonFundamentalsMay2023/5.Lists_advanced/2.Exercises/9.Anonymous_threat.py
@@ -1,43 +1,55 @@
-def merge(words, index1, index2):  # merge strings from list by index
-    new_string = ''
+def merge(words, index1, index2):
+    if index2 >= len(words):
+        index2 = len(words) - 1
+    if index1 < 0:
+        index1 = 0
+    new_word = "".join(words[index1:index2 + 1])
+    words = words[:index1] + words[index2 + 1:]
+    if new_word != "":
+        words.insert(index1, new_word)
+    return words
 
-    for index in range(index1, index2 + 1):
-        new_string += words[index]
 
-    # for word1 in words[index1 + 1: index2 + 1]:
-    #     words.remove(word1)
+def divide(words, index, parts):  # divide string at n index into n parts
+    word = [symbol for symbol in words.pop(index)]
+    word_list = []
 
-    words[index1] = new_string
+    if parts > len(word):
+        length_part = 1
+    else:
+        length_part = int(len(word) / parts)
+    starting_index = 0
+
+    for symbol in range(parts):
+        if symbol == parts - 1:
+            word_list.append("".join(word[starting_index:]))
+            break
+        else:
+            word_list.append("".join(word[starting_index:starting_index + length_part]))
+        starting_index += length_part
+
+    for el in word_list:
+        words.insert(index, el)
+        index += 1
 
     return words
 
 
-def divide(words, index, parts):
-    pass
-
-
-user_string = input().split()
-
+threat_string = input().split()
 command = input()
 
 while command != "3:1":
-    list_command = command.split()
-    event = list_command[0]
-    start_index = int(list_command[1])
-    end_index_or_parts = int(list_command[2])
+    command = command.split()
+    action = command[0]
+    first_index = int(command[1])
+    index_or_parts = int(command[2])
 
-    if event == "merge":
-        if end_index_or_parts >= len(user_string):
-            end_index_or_parts = - 1
+    if action == "merge":
+        threat_string = merge(threat_string, first_index, index_or_parts)
 
-        if start_index >= len(user_string):
-            start_index = 0
-        merge(user_string, start_index, end_index_or_parts)
-
-    elif event == "divide":
-        pass
+    elif action == "divide":
+        threat_string = divide(threat_string, first_index, index_or_parts)
 
     command = input()
 
-
-print(" ".join(user_string))
+print(" ".join(threat_string))
